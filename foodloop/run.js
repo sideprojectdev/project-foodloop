ce021200ffb03790df300b235e22f07616ccf9e4
sinoforever <sinoforever@outlook.com>
2016-06-18 13:52:56 -0400

    can random, but html is broken

--- foodloop/run.js
+++ foodloop/run.js
@@ -1,21 +1,34 @@
-var Yelp = require('yelp');
+var coord = {latitude: 43.65860098525407, longitude: -79.39782458347145};
+var positionllll = {coords: coord};
+searchBasedOnPos(positionllll);
 
-var yelp = new Yelp({
-	consumer_key: '7y4oQGiNy2LJmLws2PmpgA',
-	consumer_secret: 'kofl0SYvJ_zqqrE9E9JEffOdQrE',
-	token: 'XGnADVk6eVQ5i9XVTNlAdclRkBrai77x',
-	token_secret: 'myJUtq6VRKy16NgSCsIJKMCRgKE',
-});
+function searchBasedOnPos(position) {
 
-// See http://www.yelp.com/developers/documentation/v2/search_api
-yelp.search({ term: 'Chinese', location: 'Toronto', cll: '43.65939577525231,-79.39831190185855' })
-.then(function (data) {
-	var len = data.businesses.length
-	for(var i = 0; i < len; i++) {
-		console.log(data.businesses[i].name);
-	}
-	console.log('----------------------------------------');
-})
-.catch(function (err) {
-	console.error(err);
-});
+	var lat = position.coords.latitude;
+	var logi = position.coords.longitude;
+
+	var Yelp = require('yelp');
+
+	var yelp = new Yelp({
+		consumer_key: '7y4oQGiNy2LJmLws2PmpgA',
+		consumer_secret: 'kofl0SYvJ_zqqrE9E9JEffOdQrE',
+		token: 'XGnADVk6eVQ5i9XVTNlAdclRkBrai77x',
+		token_secret: 'myJUtq6VRKy16NgSCsIJKMCRgKE',
+	});
+
+	// See http://www.yelp.com/developers/documentation/v2/search_api
+	var off = String(Math.round(Math.random() * 500));
+	yelp.search({ term: 'restaurant', limit: '1', offset: off,
+				bounds: String(lat - 0.01) + ',' + String(logi - 0.01) + '|' +
+				String(lat + 0.01) + ',' + String(logi + 0.01)})
+	.then(function (data) {
+		var len = data.businesses.length
+		for(var i = 0; i < len; i++) {
+			console.log(data.businesses[i].name);
+		}
+		console.log('----------------------------------------');
+	})
+	.catch(function (err) {
+		console.error(err);
+	});
+}
